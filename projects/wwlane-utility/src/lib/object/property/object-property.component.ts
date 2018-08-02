2bd71615246c7c8d37b16cb3517a6ac7bde4f1e3
Mike Roberts <Mike.Roberts@wwlane.com>
2018-08-01 20:27:27 -0500

    Moved files around

---
+++ projects/wwlane-utility/src/lib/object/property/object-property.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, PipeTransform } from '@angular/core';
+
+import { ObjectPropertyService } from './object-property.service';
+
+@Component({
+	selector: 'wwl-util-object-property',
+	templateUrl: './object-property.component.html',
+	styleUrls: ['./object-property.component.css']
+})
+export class ObjectPropertyComponent {
+	@Input() wwlObject: Object;
+	@Input() wwlPropertyName: string;
+	@Input() wwlPipe?: PipeTransform;
+	@Input() wwlPipeParameters?: Object[];
+	@Input() wwlUnknownValue?: string;
+
+	constructor(private objectPropertyService: ObjectPropertyService) { }
+
+	getValue(): Object {
+		return this.objectPropertyService.retrievePropertyByName(this.wwlObject, this.wwlPropertyName, this.wwlUnknownValue, this.wwlPipe, this.wwlPipeParameters);
+	}
+
+	valueIsArray(): boolean {
+		// TODO: Don't recall getValue
+		return Array.isArray(this.getValue());
+	}
+}
---
+++ projects/wwlane-utility/src/lib/object/property/object-property.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, PipeTransform, Inject } from '@angular/core';
+
+import { ObjectPropertyService as CoreObjectPropertyService } from 'wwlane-core-utility';
+import { DEFAULT_OBJECT_PROPERTY_UNKNOWN_VALUE } from '../../config/config';
+
+@Injectable()
+export class ObjectPropertyService {
+	private defaultUnknownValue = 'Unknown';
+
+	// constructor(@Inject(DEFAULT_OBJECT_PROPERTY_UNKNOWN_VALUE) systemDefaultUknownValue: string) {
+	constructor(@Inject(DEFAULT_OBJECT_PROPERTY_UNKNOWN_VALUE) systemDefaultUknownValue: string, private coreObjectPropertyService: CoreObjectPropertyService) {
+			if (systemDefaultUknownValue !== undefined) {
+			this.defaultUnknownValue = systemDefaultUknownValue;
+		}
+	}
+
+	setDefaultUnknownValue(unknownValue: string) {
+		this.defaultUnknownValue = unknownValue;
+	}
+
+	private modifyValueForDisplay(value: Object | string, unknownValue?: string, pipe?: PipeTransform, additionalPipeArguments?: Object[]): Object | string {
+		if (!this.isAccessible(value)) {
+		// 	value = value.getDisplayValue();
+		} else if (value === null) {
+			value = (unknownValue === undefined) ? this.defaultUnknownValue : unknownValue;
+		} else if (pipe !== undefined) {
+			let pipeArguments = null;
+
+			if (additionalPipeArguments !== undefined) {
+				pipeArguments = Object.assign([], additionalPipeArguments);
+
+				pipeArguments.unshift(value);
+			} else {
+				pipeArguments = [value];
+			}
+
+			value = pipe.transform.apply(null, pipeArguments);
+		}
+
+		return value;
+	}
+
+	private modifyValuesForDisplay(values: Object[] | string[], unknownValue?: string, pipe?: PipeTransform, additionalPipeArguments?: Object[]): (Object | string)[] {
+		const response: (Object | string)[] = [];
+		for (const value of values) {
+			response.push(this.modifyValueForDisplay(value, unknownValue, pipe, additionalPipeArguments));
+		}
+
+		return response;
+	}
+
+	retrievePropertyByName(object: Object, propertyName: string, unknownValue?: string, pipe?: PipeTransform, additionalPipeArguments?: Object[]): Object | string {
+		const propertyValue = this.coreObjectPropertyService.retrievePropertyByName(object, propertyName);
+
+		let response: Object | Object[] | string[] | string;
+
+		if (Array.isArray(propertyValue)) {
+			response = this.modifyValuesForDisplay(propertyValue, unknownValue, pipe, additionalPipeArguments);
+		} else {
+			response = this.modifyValueForDisplay(propertyValue, unknownValue, pipe, additionalPipeArguments);
+		}
+
+		return response;
+	}
+
+	// parseFunctionCallUsingObject = function(object: Object, functionCall: FunctionCall, unknownValue?: string, pipe?: PipeTransform, additionalPipeArguments?: Object[]) {
+	// 	const functionCallResult = this.coreObjectPropertyService.parseFunctionCallUsingObject(object, functionCall);
+
+	// 	return this.modifyValueForDisplay(functionCallResult, unknownValue, pipe, additionalPipeArguments);
+	// };
+
+	isAccessible(value: Object | string): boolean {
+		return true; // !(value instanceof InaccessibleProperty);
+	}
+}
