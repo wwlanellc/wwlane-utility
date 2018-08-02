12fc9cf38ce5e079ca4ad853ef4a88d0ad05bf42
Mike Roberts <Mike.Roberts@wwlane.com>
2018-08-02 12:29:31 -0500

    removed uneeded exports

--- projects/wwlane-utility/src/public_api.ts
+++ projects/wwlane-utility/src/public_api.ts
@@ -1,12 +1,7 @@
-import { ENVIRONMENT_CODE_HANDLE_LOCAL } from './lib/config/config';
-
 /*
  * Public API Surface of wwlane-utility
  */
 
-export * from './lib/config/config';
+export { ENVIRONMENT_CODE_HANDLE_LOCAL, ENVIRONMENT_CODE_HANDLE_DEVELOPMENT, ENVIRONMENT_CODE_HANDLE_PRODUCTION } from './lib/config/config';
 export * from './lib/wwlane-utility.module';
 export * from './lib/object/property/object-property.service';
-export * from './lib/object/property/object-property.component';
-export * from './lib/directive/loading-if.directive';
-export * from './lib/directive/important-class.directive';
