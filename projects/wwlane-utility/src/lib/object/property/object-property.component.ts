0add1b161daf50171437aad21363230fb3e60d40
Mike Roberts <Mike.Roberts@wwlane.com>
2018-08-06 13:12:56 -0500

    use ngOnChanges to set the value

--- projects/wwlane-utility/src/lib/object/property/object-property.component.ts
+++ projects/wwlane-utility/src/lib/object/property/object-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, PipeTransform } from '@angular/core';
+import { Component, Input, PipeTransform, OnChanges } from '@angular/core';
 
 import { ObjectPropertyService } from './object-property.service';
 
@@ -7,21 +7,26 @@ import { ObjectPropertyService } from './object-property.service';
 	templateUrl: './object-property.component.html',
 	styleUrls: ['./object-property.component.css']
 })
-export class ObjectPropertyComponent {
+export class ObjectPropertyComponent implements OnChanges {
 	@Input() wwlObject: Object;
 	@Input() wwlPropertyName: string;
 	@Input() wwlPipe?: PipeTransform;
 	@Input() wwlPipeParameters?: Object[];
 	@Input() wwlUnknownValue?: string;
 
+	private currentValue: Object;
+
 	constructor(private objectPropertyService: ObjectPropertyService) { }
 
+	ngOnChanges() {
+		this.currentValue = this.objectPropertyService.retrievePropertyByName(this.wwlObject, this.wwlPropertyName, this.wwlUnknownValue, this.wwlPipe, this.wwlPipeParameters);
+	}
+
 	getValue(): Object {
-		return this.objectPropertyService.retrievePropertyByName(this.wwlObject, this.wwlPropertyName, this.wwlUnknownValue, this.wwlPipe, this.wwlPipeParameters);
+		return this.currentValue;
 	}
 
 	valueIsArray(): boolean {
-		// TODO: Don't recall getValue
-		return Array.isArray(this.getValue());
+		return Array.isArray(this.currentValue);
 	}
 }
